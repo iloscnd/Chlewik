9f941568af69a3c32f80cebf9908eff7a4eed0f0
iloscnd <iloscnd@gmail.com>
2017-01-19 14:24:49 +0100

    szkielet

--- site/app.js
+++ site/app.js
@@ -1,5 +1,33 @@
 
 
+var http = require('http');
+var express = require('express');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+
+app.get("/", (req, res) =>{
+
+    res.render(index.ejs);
+
+});
+
+
+app.use((req,res,next) => {
+
+    res.render('404.ejs', { url : req.url });
+
+});
+
+
+
+var server = http.createServer(app).listen(process.env.PORT || 3000);
+
+console.log( 'serwer started' );
+
 
 
 

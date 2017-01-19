21fa3b3e669405b8822733e2aba5a2aea25a68f2
iloscnd <iloscnd@gmail.com>
2017-01-19 14:56:19 +0100

    add more npm modules

--- site/app.js
+++ site/app.js
@@ -1,12 +1,17 @@
-
-
 var http = require('http');
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');//mozna spróbować szyfrować
 
 var app = express();
 
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use( bodyParser.urlencoded({extended:true}) ) ;
+app.use( express.static('./static'));
+app.use( cookieParser() );
 
 
 app.get("/", (req, res) =>{

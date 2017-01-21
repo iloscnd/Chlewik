843e9b0fdcfb054fe2ee84444218072f285bdea8
iloscnd <iloscnd@gmail.com>
2017-01-21 17:01:02 +0100

    forgot

--- site/app.js
+++ site/app.js
@@ -62,6 +62,16 @@ app.post("/guest",(req,res)=>{
 });
 
 
+app.get('/register', (req,res) =>{
+    res.render('register.ejs');
+});
+
+app.get('/login', (req,res) =>{
+    res.render('login.ejs');    
+});
+
+
+
 app.get('/test', function (req, res) {
   if (req.session.views) {
     req.session.views++;

5683d58e7f9f9c859bb306b7ee65ff381d7e05c8
iloscnd <iloscnd@gmail.com>
2017-01-23 06:02:54 +0100

    kinda working game

--- site/game.js
+++ site/game.js
@@ -8,13 +8,22 @@ router.use( express.static('./static'));
 
 var returnRouter = function(io,session) {
 
+    var state = [0,0,0,0,0,0,0,0,0];
+    var player = [undefined, undefined];
+    var turn = 0;   
+    var srcs = ["tictac/empty.png","tictac/tic.png","tictac/tac.png"]
+    var end = 0;
+
     router.get('/', function(req, res){
+        
+       
+
         if(!req.session.entered)
         {
             res.redirect("/");
         }
         else
-            res.render('game.ejs');
+            res.render('game.ejs',{state : state, srcs:srcs});
     });
 
 
@@ -23,20 +32,63 @@ var returnRouter = function(io,session) {
         session(socket.request, socket.request.res, next); //wtedy moge sie dostac do sesji w socket
     });
 
-
+    
     io.on('connection', function(socket){
        // console.log(socket);
-        console.log('A socket with sessionID ' + socket.client.id + ' connected!');
+       // console.log('A socket with sessionID ' + socket.client.id + ' connected!');
+
+        if(!player[0])
+            player[0] = socket.request.session.name;
+        else
+            if(!player[1] && player[0] != socket.request.session.name)
+                player[1] = socket.request.session.name;
 
         socket.on('FieldClicked',function(msg){
-            console.log("Socket with id " + socket.client.id + " clicked filed number " + msg);
+           // console.log("Socket with id " + socket.client.id +" and name " + socket.request.session.name +  " clicked filed number " + msg);
+            
+            if(state[msg] == 0 && !end){
+                if(player[turn%2] == socket.request.session.name){
+                    io.emit('change',[msg,turn%2]);
+                    state[msg] = turn%2 + 1;
+                    turn++;
+                    //check if won
+
+                    won = false;
+                    for( i = 0; i<3; i++)
+                        if( state[i] != 0 && state[i]==state[i+3] && state[i]==state[i+6])
+                            won = true;
+                    for( i = 0; i<3; i++)
+                        if( state[3*i] != 0 && state[3*i]==state[3*i+1] && state[3*i]==state[3*i+2])
+                            won = true;
+                    if( state[0] != 0 && state[0]==state[4] && state[0]==state[8])
+                        won = true;
+                    if( state[2] != 0 && state[2]==state[4] && state[4]==state[6])
+                        won = true;
+                    
+                    if(won){
+                        //console.log(state);
+                        io.emit('won', socket.request.session.name);
+                        end = 1;
+                    }
+                }
+            }
+
         });
+
+        socket.on('reset', function(msg){
+            for(i=0; i<9; i++){
+                io.emit('clear',i)
+                state[i] = 0;
+            }
+            end = 0;
+        });
+
 
         socket.on('disconnect', function(){
                 console.log('A socket with sessionID ' + socket.client.id + ' disconnected!');
-                
+                //check if disconnected user was a player(and is not having any kind of shit)
         });
-        console.log(socket.request.session.name);
+       // console.log(socket.request.session.name);
     });
 
 

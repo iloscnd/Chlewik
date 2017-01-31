d9b17d47036151577fc34b233f12db81baa360ef
iloscnd <iloscnd@gmail.com>
2017-01-31 14:01:58 +0100

    to naprawie rooms?

--- site/user.js
+++ site/user.js
@@ -64,21 +64,6 @@ var routerFun = function(userz,id) {
             return; //a może by res.end()?           
         });
 
-
-      /*  if (userz.get(name) != undefined &&  userz.get(name).pwd == pwd) flag = true;
-
-        if (flag) {
-            req.session.legit.entered = 1;
-            req.session.name = name;
-
-            req.session.personID = userz.get(name).id;
-
-            req.session.guest = 0;
-            res.redirect("/rooms");
-            return; //a może by res.end()?
-        }
-    else { res.redirect('/redirectDefault'); return; /*a może by res.end()?*/ //} //to jakby ktoś wklepał dane inaczej (wysłał straszliwy html np.) i nie przedzedł przez formularz sprawdzający
-        //co prawda w ten sposób sprawdza się 2 razy :/
     });
 
     router.post('/create', (req,res) =>{
@@ -129,26 +114,6 @@ var routerFun = function(userz,id) {
 
 
         //console.log("zakładam\n");
-        /*
-        if (userz.get(name) != undefined) flag = false;
-        if (flag) {
-            var theID = id;
-            ++id;
-            var newUser = {
-                id : theID,
-                name : name, //powielam, ale niech będzie
-                pwd : req.body.pwd
-            };
-            req.session.personID = theID;
-            
-            userz.set(name,newUser);
-            req.session.legit.entered = 1;
-            req.session.name = req.body.name;
-            req.session.guest = 0;
-            res.redirect('/rooms');
-            return; //a może by res.end()?
-        }
-        else { res.redirect('/redirectDefault');return; /*a może by res.end()?*/// } //to jakby ktoś wklepał dane inaczej (wysłał straszliwy html np.) i nie przedzedł przez formularz sprawdzający
         });
 
 
@@ -177,15 +142,6 @@ var routerFun = function(userz,id) {
             return; //a może by res.end()?           
         });
  
-
-/*using users
-        if (userz.get(name) != undefined) flag = false;
-        var resp = "";
-        if (flag) resp="OK"; else resp="NO";
-        //console.log(resp+"\n");
-        res.send(resp);
-        return; //a może by res.end()?
-*/    
     });
 
     router.post('/ajaxValid', (req,res) => { //zmienić jakoś na post
@@ -220,16 +176,6 @@ var routerFun = function(userz,id) {
             }
             return; //a może by res.end()?           
         });
-    /*    if (userz.get(name) != undefined) getPwd = userz.get(name).pwd;
-        //console.log(userz.get(name)+"\n");
-        if(getPwd != undefined) {
-            if (getPwd == pwd) resp = "OK"; else resp = "BAD";
-            flag = false; 
-        }
-        if (flag) resp="BAD"; //było NOONE, ale nie powinien mówić które źle 
-        //console.log(resp+"\n");
-        res.send(resp);
-        return; //a może by res.end()? */
     });
 
     router.post('/ajaxPwd', (req,res) => {
@@ -243,29 +189,52 @@ var routerFun = function(userz,id) {
         return; //a może by res.end()?
     });
 
-    //trzeba userz, więc w routerFun - dopiero odtąd i tak mają być potrzebne uprawnienia, a nie wcześniej
+    //trzeba usersz, więc w routerFun - dopiero odtąd i tak mają być potrzebne uprawnienia, a nie wcześniej
     // !!! tu już te do których trzeba być zalogowanym
     router.use('/', (req,res,next) => {
 
-        
-        //usuwanie nieaktualnych uprawnień, żeby nie próbowało przekierować i się nie pętliło - usuwamy tylko entered, bo resztę się i tak ustawi przy ponownym
-        //trzeba usunąć też następne poziomy, bo już potem tam nie dotrze żeby je usunąć
-        if( req.session.legit.entered && !req.session.guest && userz.get(req.session.name) == undefined) { /*console.log("usuwam że zalogowany");*/ delete req.session.legit.entered; delete req.session.legit.roomEntered; delete req.session.legit.inGame; req.session.save(); } //B. WAŻNE!!! 
-        if ( req.session.legit.entered && !req.session.guest) {
-            var user = userz.get(req.session.name);
-            if (user != undefined && user.id != req.session.personID) { /*console.log("usuwam że zalogowany");*/ delete req.session.legit.entered; delete req.session.legit.roomEntered; delete req.session.legit.inGame; req.session.save(); } //B. WAŻNE!!!
-        }
-        //starczy wykasowywać tu w sprawdzaniu uprawnień te nieaktualne, też z ID
-        
+        if(!req.session.guest){
+            //var user = userz.get(req.session.name);
+            client.query( "SELECT id FROM users WHERE name = '" + req.session.name + "';",function(err, result){
+                if(err)
+                    console.log(err);
+                
+                console.log(result);
+                if(!result) { /*console.log("usuwam że zalogowany");*/ 
+                    delete req.session.legit.entered; 
+                    delete req.session.legit.roomEntered; 
+                    delete req.session.legit.inGame; 
+                    req.session.save(); 
+                }
 
-        var ses = req.session;
-        if (!ses.legit.entered || ses.guest) { 
-            //console.log(ses.legit.entered+"WRACAM2");
-            res.redirect('/'); 
-            return; 
+                else if( req.session.legit.entered && !req.session.guest && result.rowCount == 0) { /*console.log("usuwam że zalogowany");*/ 
+                    delete req.session.legit.entered; 
+                    delete req.session.legit.roomEntered; 
+                    delete req.session.legit.inGame; 
+                    req.session.save(); 
+                } //B. WAŻNE!!! 
+                
+                else if ( req.session.legit.entered && !req.session.guest) {
+                    if (result.rowCount != 0 && result.rows[0].id != req.session.personID) { /*console.log("usuwam że zalogowany");*/ 
+                        delete req.session.legit.entered; 
+                        delete req.session.legit.roomEntered; 
+                        delete req.session.legit.inGame; 
+                        req.session.save(); 
+                    } //B. WAŻNE!!!
+                }
+
+                var ses = req.session;
+                if (!ses.legit.entered) { 
+                    //console.log(ses.legit.entered+"WRACAM3");
+                    res.redirect('/'); 
+                    return; 
+                }
+
+                req.session.urlLegit.entered = ses.legit.entered; //1, ale tak bezpiecznie
+                
+                next();
+            });
         }
-        req.session.urlLegit.entered = ses.legit.entered; //1, ale tak bezpiecznie
-        next();
     });
 
     router.all('/settings', (req,res) => { //przeszedł przez middleware, czyli uprawnienia są

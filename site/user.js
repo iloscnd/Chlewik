56dd39959b8396e59d4da54c7b8b43abe7b62a8b
iloscnd <iloscnd@gmail.com>
2017-01-31 05:16:14 +0100

    forgot comma

--- site/user.js
+++ site/user.js
@@ -90,7 +90,7 @@ var routerFun = function(userz) {
                     req.session.legit.entered = 1;
                     req.session.name = req.body.name;
                     req.session.guest = 0;
-                    var insertQuery = client.query( "INSERT INTO users (id, name, pass) VALUES (1, '" + name + "' '" + req.body.pwd + "')");
+                    var insertQuery = client.query( "INSERT INTO users (id, name, pass) VALUES (1, '" + name + "', '" + req.body.pwd + "')");
                     insertQuery.on('end',function(){
                         res.redirect('/rooms');
                         return; //a może by res.end()?
